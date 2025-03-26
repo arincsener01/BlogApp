a142316844696167fdfd74cad0e8f38b76971d3a
Arinc Sener <senerar-nc@windowslive.com>
2025-03-26 12:24:20 +0300

    blog is done

--- APP.BLOG/Domain/BlogDb.cs
+++ APP.BLOG/Domain/BlogDb.cs
@@ -4,7 +4,13 @@ namespace APP.BLOG.Domain
 {
     public class BlogDb : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Tag> Tags { get; set; }
         public DbSet<BlogTag> BlogTags { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<UserSkill> UserSkills { get; set; }
         public BlogDb(DbContextOptions<BlogDb> options) : base(options)
         {
         }
--- APP.BLOG/Domain/BlogTag.cs
+++ APP.BLOG/Domain/BlogTag.cs
@@ -10,5 +10,7 @@ namespace APP.BLOG.Domain
         public int Id { get; set; }
         public int BlogId { get; set; }
         public int TagId { get; set; }
+        public Blog Blog { get; set; }
+        public Tag Tag { get; set; }
     }
 }
